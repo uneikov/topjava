35f9975d742daa10794ff7d532ddc7f96e36cf40
IOURI NEIKOV <neikov@gmail.com>
2016-09-07 18:20:49 +0300

    prepare to HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -17,13 +17,11 @@ public class UserMealsUtil {
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
 
-        // Summarize calories for each day
         Map<LocalDate, Integer> caloriesPerDayMap = mealList
                 .parallelStream()
                 .collect(Collectors.toConcurrentMap(meal -> meal.getDateTime().toLocalDate(),
                 UserMeal::getCalories, (mealCalories1, mealCalories2) -> mealCalories1 + mealCalories2));
 
-        // Populate result list
         return mealList
                 .parallelStream()
                 .filter(meal -> TimeUtil.isBetween(meal.getDateTime().toLocalTime(), startTime, endTime))
